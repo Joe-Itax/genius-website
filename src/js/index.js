71a42fa7331930f9e91ab10943b65e8415699db5
Joe-Itax <josephitakala18@gmail.com>
2023-12-18 19:18:01 +0000

    update: make the navbars of all pages the same

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,27 @@ const showMenu = () => {
 
 showMenu();
 
+
+const onScrollUpdate = () => {
+  let lastScroll = 0;
+
+  window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+      body.classList.remove('scroll-up');
+    }
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+      body.classList.remove("scroll-up");
+      body.classList.add("scroll-down");
+    }
+
+    if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+      body.classList.remove("scroll-down");
+      body.classList.add("scroll-up");
+    }
+    lastScroll = currentScroll;
+  })
+}
+
+onScrollUpdate();
+
