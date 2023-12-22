4645e2f9dbec2afa7168a6153e1f1aba7cad60f1
Joe-Itax <josephitakala18@gmail.com>
2023-12-22 17:46:58 +0000

    update: the onScrollUpdate function

--- src/js/index.js
+++ src/js/index.js
@@ -14,26 +14,30 @@ const showMenu = () => {
 showMenu();
 
 
+// onScrollUpdate();
 const onScrollUpdate = () => {
   let lastScroll = 0;
+  const scrollThreshold = 200; // 200px
 
   window.addEventListener('scroll', () => {
     const currentScroll = window.pageYOffset;
-    if (currentScroll <= 0) {
-      body.classList.remove('scroll-up');
-    }
-    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-      body.classList.remove("scroll-up");
-      body.classList.add("scroll-down");
-    }
 
-    if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-      body.classList.remove("scroll-down");
-      body.classList.add("scroll-up");
+    // Activer uniquement si le scroll dépasse 200px
+    if (currentScroll >= scrollThreshold) {
+      if (currentScroll <= 0) {
+        body.classList.remove('scroll-up');
+      } else if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-up");
+        body.classList.add("scroll-down");
+      } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-down");
+        body.classList.add("scroll-up");
+      }
+      lastScroll = currentScroll;
     }
-    lastScroll = currentScroll;
-  })
+  });
 }
 
 onScrollUpdate();
+
 
